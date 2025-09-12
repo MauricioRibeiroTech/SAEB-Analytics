4c3e372b6eb7874fe6d689be504a2700e57abf0b
MAURICIO A. RIBEIRO <ribeiro.mauricio1601@gmail.com>
2025-09-12 11:53:13 -0300

    Update 1_SAEB_Metodologia.py

--- pages/1_SAEB_Metodologia.py
+++ pages/1_SAEB_Metodologia.py
@@ -60,8 +60,10 @@ def calcular_porcentagens(df, disciplina):
             'Sim1': 10, 'Sim2': 10, 'Sim3': 12,
             'Sim4': 15, 'Sim5': 18, 'Sim6': 18,
             'Sim7': 20, 'Sim8': 16, 'Sim9': 26,
-            'Sim10': 16, 'Sim11': 18, 'Sim12': 16,
-            'Sim13': 16,'Sim14': 23,'Sim15': 16,'Sim16': 24,'Sim17': 25, 'Sim18':16, 'Sim19':26
+            'Sim10': 16,'Sim11':18,'Sim12': 16,
+            'Sim13': 16,'Sim14': 23,'Sim15': 16,
+            'Sim16': 24,'Sim17': 25, 'Sim18':16,
+            'Sim19':26
 
         },
         "Português": {
@@ -69,7 +71,9 @@ def calcular_porcentagens(df, disciplina):
             'Sim4': 15, 'Sim5': 18, 'Sim6': 18,
             'Sim7': 14, 'Sim8': 16, 'Sim9': 26,
             'Sim10': 16, 'Sim11': 18, 'Sim12': 16,
-            'Sim13': 16,'Sim14': 16,'Sim15': 16,'Sim16': 15,'Sim17': 15, 'Sim18':16, 'Sim19':26
+            'Sim13': 16,'Sim14': 16,'Sim15': 16,
+            'Sim16': 15,'Sim17': 15, 'Sim18':16, 
+            'Sim19':26
 
         }
     }
@@ -386,6 +390,7 @@ if 'Componente' in df.columns and len(df['Componente'].unique()) > 1:
 
 
     st.plotly_chart(fig_comp, use_container_width=True)
+
 
 
 
